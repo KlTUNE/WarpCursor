c25e7ccf31c5519495e6895af9ad184aae33c410
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-02-23 12:15:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ BORDER = 0
 while True:
     cursor_position = pyautogui.position()
     print(cursor_position, flag)
+
     if cursor_position.x >= BORDER and flag == 1:
         flag = 0
-        # pyautogui.moveTo(cursor_position.x, 800)
+        if cursor_position.y < 420: pyautogui.moveTo(cursor_position.x, (cursor_position_old.y+1080) * (1079/1500))
+        else: pyautogui.moveTo(cursor_position.x, 1079)
+
     elif cursor_position.x < BORDER and flag == 0:
         flag = 1
-        if cursor_position.y >= 0:
-            # aa = 400
-            aa = int((cursor_position.y*(1727/1079)) - 1080)
-            pyautogui.moveTo(cursor_position.x, aa)
-        else:
-            pyautogui.moveTo(cursor_position.x, -1080)
+        if cursor_position.y >= 0: pyautogui.moveTo(cursor_position.x, cursor_position_old.y*(1500/1079) - 1080)
+        else: pyautogui.moveTo(cursor_position.x, -1080)
+    cursor_position_old = cursor_position
